99b84fa97106ce7a92bff469f823f41ab724292b
Jannat <jannat3ahmed@gmail.com>
2024-09-11 17:17:31 +0100

    Project added in local repository

---
+++ src/test/java/com/microtech/EasyBuy/EasyBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microtech.EasyBuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
