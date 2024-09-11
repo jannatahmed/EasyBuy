48abcc4d6f0d635805580e0636eb5d1cfb2c251e
Jannat <jannat3ahmed@gmail.com>
2024-09-11 18:07:10 +0100

    create a controller

--- src/main/java/com/microtech/EasyBuy/controller/EmployeeController.java
+++ src/main/java/com/microtech/EasyBuy/controller/EmployeeController.java
@@ -1,6 +1,12 @@
 package com.microtech.EasyBuy.controller;
 
+import com.microtech.EasyBuy.entity.Employee;
+import com.microtech.EasyBuy.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/emp")
 public class EmployeeController {
 @Autowired
-
+    EmployeeRepository employeeRepository;
+@PostMapping("add/emp")
+    public ResponseEntity<String> addEmployee(@RequestBody Employee employee){
+    employeeRepository.save(employee);
+    return new ResponseEntity<>("Employee added successfully", HttpStatus.CREATED);
+}
 }
