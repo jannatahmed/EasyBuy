0b4df680ff984f68de543538176f4ba7facfd7c9
Jannat <jannat3ahmed@gmail.com>
2024-09-11 17:50:47 +0100

    Create a new packge

--- src/main/java/com/microtech/EasyBuy/entity/Employee.java
+++ src/main/java/com/microtech/EasyBuy/entity/Employee.java
@@ -2,6 +2,7 @@ package com.microtech.EasyBuy.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Employee {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private int id;
     private String name;
